380e78dc210a3450ba7931434e9dd053cbf4e72c
Draaft4 <draaft@outlook.com>
2023-05-08 10:04:23 -0500

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name':'loyalty web serrvice',
+    'author':'Sebastian bedoya and Pedro neira',
+    'website':'enlance git',
+    'summary':'conexion y envio de variables entre odoo y flutter',
+
+}
---
+++ models/variables_card.py
@@ -0,0 +1,5 @@
+from odoo import api, fields, models, _, tools
+
+class variablesflu(models.Model):
+    _name = "variables.flu"
+    
