905c2e9d74485818c277efbb3fa876095e9b6fc3
bedoya1207 <mbedoyao@est.ups.edu.ec>
2023-06-19 17:39:16 -0500

    nuevo cambio

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,14 +4,14 @@ import json
 
 class getClientData(http.Controller):
 
-    @http.route('/api/clientData', auth='public', website=False, crf=False, type='http', methods=['GET', 'POST'])
+    @http.route('/api/clientData', auth='public', website=False, crf=False, type='http', methods=['GET'])
     def get_data(self, **kw):
         costumerList = []
-        costumers = http.request.env['res.partner'].sudo().search([])
+        costumers = http.request.env['loyalty.card'].sudo().search([])
         for costumer in costumers:
             costumerList.append({
-                'id': costumer.id,
-                'name': costumer.name,
+                'partner_id': costumer.partner_id.name,
+                'points': costumer.points,
 
             })
 
