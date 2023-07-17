300a2333bf6609079f172d01133cdc06d39bc4a7
Draaft4 <draaft@outlook.es>
2023-07-17 11:15:51 -0500

    Envio de notificaciones al momento de compra

--- models/loyalty_comm.py
+++ models/loyalty_comm.py
@@ -1,4 +1,6 @@
 from odoo import models, api
+import firebase_admin
+from firebase_admin import credentials, messaging
 
 class CustomLoyalty(models.Model):
     _inherit="loyalty.card"
@@ -8,8 +10,20 @@ class CustomLoyalty(models.Model):
         res = super(CustomLoyalty, self).write(vals)
         if not self.env.context.get('loyalty_no_mail', False) and 'points' in vals:
             points_before = {coupon: coupon.points for coupon in self}
-            print(points_before)
         if not self.env.context.get('loyalty_no_mail', False) and 'points' in vals:
             points_changes = {coupon: {'old': points_before[coupon], 'new': coupon.points} for coupon in self}
-            print(points_changes)
+        if not self.env.context.get('loyalty_no_mail', False) and 'points' in vals:
+            partner = self.partner_id
+            points = self.points
+            if(partner.token_ids):
+                for token in partner.token_ids:
+                    message = messaging.Message(
+                    notification=messaging.Notification(
+                        title="Has Ganado Puntos!",
+                        body=f"Usted ha ganado ${points}, gracias por su compra.",
+                    ),
+                    token=token.token,
+                )
+                response = messaging.send(message)
+                print('Successfully sent message:', response)
         return res
